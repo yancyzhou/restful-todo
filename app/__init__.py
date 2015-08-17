b9e57b9b939bd9337f8947e7476a048a333c2349
yancyzhou <python@vip.126.com>
2015-08-17 14:32:43 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,14 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import config
-
+from flask.ext.restful import Api,Resource,reqparse
 bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
 
 def create_app(config_name):
   app = Flask(__name__)
+  api = Api(app)
   app.config.from_object(config[config_name])
   config[config_name].init_app(app)
 
